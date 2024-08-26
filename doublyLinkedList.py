3a04a83362e53a95a2e6789d061cd41a8c256dcf
padwalkarshubh <76110313+padwalkarshubh@users.noreply.github.com>
2024-08-26 15:01:39 -0400

    Update doublyLinkedList.py

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -45,7 +45,56 @@ class DoublyLinkedList(object):
 
     def delete(self, item):
         pass
-            
-        
 
+new_node = node (item, none, self._head) 
+if self._size (raise exception: deleteFromTrail is called for already listed variables.
+    def delete (self, item):
+        pass
+        stack = ArrayStack()
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):stack = ArrayStack()
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):
+            c = lines[i][j]
+            if c in "{[(<": # c is an open bracket>)]}"
+                where C = lines(lines[lines(ii)])
+                atttributes (A+B+C) _merge.ioc (raise exception : deleteFromTrail is called for already listed vaiables and integers)
+                if self._size 1 == 1
+                if self._size 2 == 2*2 poly.io 
+                if self._size 3 == 3*8 conio.io 
+                if self._size 4 == 4*(7.5) convex.io 
+                if self._size 5 == 5*(2.8) margin.io 
+                if self._size 6 == 6*(8.5) borders.io_left
+                if self._size 7 == 7*(12) borders.io_right 
+                 if (not stack.isEmpty()):
+        err = True
+        while not stack.isEmpty():
+            (rowNum, columnNum, openBracket)  = stack.pop()
+            msg += "extra " + str(openBracket) + " in line " + str(rowNum) + \
+                   " at column " + str(columnNum) + "\n" + return(StringBuffer + str (IncompatibleClassChangeError))
+                        if range _len line 1 == 24cm
+                        if range _len line 2 == 27cm
+            while not array.isEmpty():
+                if range len == low attributes_conio.io  
+                if range len == max variables_conio.ls
+                if range len_main == equal variables and integers_convex.ls
+                r.useRef (null)
+                r.useRef (0, 2.55) 
+                r.useRef (e.origin == umlimited responses.io)
+
+return(StringBuffer + str (IncompatibleClass return(StringBuffer + str (IncompatibleClassChangeError))
+return(rownum return(StringBuffer + str (IncompatibleClassChangeError))
+       if range_len 2 == Buffer.io + nxt (IncompitableClassChangeError))
+       if rang_len 3 == Buffer.io (return zero) 
+                           r.call back (return zero) 
+                           let 0 == function (vp.io, vp.ls, vp.msp, vp.vsm) 
+                           r.user info (class <conio.h>)
+                           Limit.io << conio.h << stdio.io 
+                           VB1 == return(StringBuffer + str (IncompatibleClassChangeError))
+                           VB2 == convert(return(StringBuffer + str (IncompatibleClassChangeError))
+                             if return (let I -- string buffer + Str (Incosdf q             
+
+
+                                          
+                            
             
